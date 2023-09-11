13ec405a3617240a709a4155f5922cee306ee9be
sj00coder <jshubham94110@gmail.com>
2023-09-11 10:14:32 +0530

    initial commit frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Box, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Box>
+        <Text> We are good to go</Text>
+      </Box>
+    </>
+  );
+}
+
+export default App;
